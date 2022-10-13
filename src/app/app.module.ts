e442085cf05aa828f22629893df83394398642ad
javierecs <javier.cuellar@udem.edu>
2022-10-13 15:41:53 -0500

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarComponent } from './agregar/agregar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarComponent
   ],
   imports: [
     BrowserModule,
