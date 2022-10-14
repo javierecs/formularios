635b401841e082cf6885a1eda90d437e070e609d
javierecs <javier.cuellar@udem.edu>
2022-10-14 10:48:52 -0500

    se agregaron formularios reactivos

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  formularioCreado!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.crearFormulario();
+  }
+
+  crearFormulario(){
+    this.formularioCreado = new FormGroup({
+      nombre: new FormControl(null, Validators.required),
+      correo: new FormControl(null, [Validators.required, Validators.email]), 
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    })
+    //this.fb.group({
+    //   nombre: [null, Validators.required],
+    //   correo: [null, Validators.compose([
+    //     Validators.required,
+    //     Validators.email
+    //   ])],
+    //   password: [null, Validators.compose([
+    //     Validators.required,
+    //     Validators.minLength(8)
+    //   ])]
+    // })
+  }
+
+  agregar(){
+    console.log(this.formularioCreado.value)
   }
 
 }
