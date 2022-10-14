d10c5961a97130f85427b61ca7b6995e1d250172
javierecs <javier.cuellar@udem.edu>
2022-10-14 12:58:18 -0500

    Se hizo formulario reactivo

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+interface Usuario{
+  nombre: string;
+  correo: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
@@ -9,8 +15,11 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class AgregarComponent implements OnInit {
 
   formularioCreado!: FormGroup;
+  usuarios: Array<Usuario> = new Array<Usuario>();
+  esNuevo: boolean = true;
+  posicionEditada: number = -1;
 
-  constructor(private fb: FormBuilder) { }
+  constructor() { }
 
   ngOnInit(): void {
     this.crearFormulario();
@@ -21,22 +30,38 @@ export class AgregarComponent implements OnInit {
       nombre: new FormControl(null, Validators.required),
       correo: new FormControl(null, [Validators.required, Validators.email]), 
       password: new FormControl(null, [Validators.required, Validators.minLength(8)])
-    })
-    //this.fb.group({
-    //   nombre: [null, Validators.required],
-    //   correo: [null, Validators.compose([
-    //     Validators.required,
-    //     Validators.email
-    //   ])],
-    //   password: [null, Validators.compose([
-    //     Validators.required,
-    //     Validators.minLength(8)
-    //   ])]
-    // })
+    });
   }
 
   agregar(){
-    console.log(this.formularioCreado.value)
+    this.usuarios.push(this.formularioCreado.value as Usuario);
+    this.formularioCreado.reset();
+  }
+
+  editar(){
+    let formulario = this.formularioCreado.value
+    this.usuarios[this.posicionEditada].nombre = formulario.nombre;
+    this.usuarios[this.posicionEditada].correo = formulario.correo;
+    this.usuarios[this.posicionEditada].password = formulario.password;
+    this.formularioCreado.reset();
+    this.esNuevo = true;
+    this.posicionEditada = -1;
+  }
+
+  editarUsuario(posicion: number){
+    this.formularioCreado.setValue({
+      nombre: this.usuarios[posicion].nombre, 
+      correo: this.usuarios[posicion].correo,
+      password: this.usuarios[posicion].password
+    });
+
+    this.posicionEditada = posicion;
+    this.esNuevo = false;
+
+  }
+
+  eliminarUsuario(posicion: number){
+    this.usuarios.splice(posicion, 1);
   }
 
 }
